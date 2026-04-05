e5ec92accfddcdfbe3a4b7cffd0dfde743bcdf27
Родион Леин <xukkubtw@gmail.com>
2026-04-05 11:45:49 +0700

    Реализован вариант №5

---
+++ src/ConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите первое число: ");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите второе число: ");
+        int b = int.Parse(Console.ReadLine());
+
+        while (b != 0)
+        {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+
+        Console.WriteLine("НОД = " + a);
+    }
+}
